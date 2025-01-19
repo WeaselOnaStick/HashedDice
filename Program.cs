ace102b792c4ba6d0a233c136753e1463de6135d
Weasel On A Stick <korotkih.00@gmail.com>
2025-01-19 12:51:42 +0400

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+
+
+class DiceGame  {
+	static void Main(string[] args) {
+		for (int i = 0; i < args.Length; i++)
+		{
+			Console.WriteLine($"Argument {i}: {args[i]}");
+		}
+	}
+}
